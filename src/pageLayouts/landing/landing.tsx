96bdc6cc030f5c3ef4f678c7341e9a9346f06e98
Arslan-95 <arslan180198@gmail.com>
2023-08-07 14:29:53 +0300

    [dev] OurPartners screen is ready

--- src/pageLayouts/landing/landing.tsx
+++ src/pageLayouts/landing/landing.tsx
@@ -3,6 +3,7 @@ import Main from './ui/main/main';
 import Features from './ui/features/features';
 import Benefits from './ui/benefits/benefits';
 import AboutUs from './ui/about-us/about-us';
+import OurPartners from './ui/our-partners/our-partners';
 
 const LandingPage = () => {
   return (
@@ -11,6 +12,7 @@ const LandingPage = () => {
       <Features />
       {/* <Benefits /> */}
       <AboutUs />
+      <OurPartners />
     </div>
   );
 };
